fc4d1b6ff8d1b1afa3d94b2bbceffac4d5420ccd
Dwayne Pink <dwaynepink@gmail.com>
2023-12-13 14:49:27 -0800

    Updated additional categories

--- main.py
+++ main.py
@@ -7,30 +7,36 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 desired_fields = ["book_title", "product_page_url", "review_rating", "category",]
 
-book_title = soup.find('h1')
+book_title_element = soup.find('h1')
+book_title = book_title_element.text.strip() if book_title_element else "Unknown Title"
 product_page_url = (URL)
 review_rating_element = soup.find('p', class_='star-rating')
 review_rating = review_rating_element['class'][1] if review_rating_element else None
+category_element  = soup.find_all('ul', class_='breadcrumb')
+category = category_element[0].find('a', href="../category/books/business_35/index.html").text.strip() if category_element else None
+description_element = soup.find('meta', attrs={'name': 'description'})
+description = description_element.get('content') if description_element else None
 
 
-print(book_title)
-print(product_page_url)
-print(review_rating)
-
-
-
-c = soup.find_all(lambda tag: tag.name == 'a' and '../category/books/poetry_23' in tag.get('href', ''))
-
+#description = soup.find('article', class_=('product_page'))
 
 
 
 
-print
 
 
-
-#review_rating = soup.find('p', class_= "star-rating Four").
 
 #print(book_title)
 #print(product_page_url)
+#print(review_rating)
+#print(category)
+#print(description)
+
+
+
+
+
+#image file contained in here
+#description = soup.find('article', class_=('product_page'))
+
 
