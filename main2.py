51baf90558b46fb5731b80adf34e2931f9c4973e
Dwayne Pink <dwaynepink@gmail.com>
2023-12-14 21:05:55 -0800

    extract URL for category. and check if there is a next link on page.

--- main2.py
+++ main2.py
@@ -6,3 +6,20 @@ start_url = "http://books.toscrape.com/catalogue/category/books/travel_2/index.h
 
 # In the event there is a next page on any category this will save pages visited
 urls_visited = []
+
+# Function to extract URLs from a page
+def extract_urls(page_url):
+    page = requests.get(page_url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+# Extract URLs for category
+    category_urls = [urljoin(page_url, link.get('href')) for link in soup.select('h3 a') if link.get('href')]
+    urls_visited.extend(category_urls)
+
+# Check if there is a "next" link on the page
+    next_page_link = soup.select_one('li.next a')
+    next_page_url = urljoin(page_url, next_page_link['href']) if next_page_link else None
+
+    return next_page_url
+
+
