0b7f61b015ff01d1a93c2e0d5a246bf16bb1cae0
Dwayne Pink <dwaynepink@gmail.com>
2023-12-22 17:02:48 -0800

    extract 'img' structure from website. You url join to extract full html to get the correct image url for extraction

--- ImageExtractor.py
+++ ImageExtractor.py
@@ -11,3 +11,10 @@ image_directory = "images"
 os.makedirs(image_directory, exist_ok=True)
 
 # Function to download images from a page
+def download_images(page_url):
+    page = requests.get(page_url, timeout=120)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # Extract image URLs from the page
+    img_elements = soup.select('img')
+    img_urls = [urljoin(page_url, img['src']) for img in img_elements]
